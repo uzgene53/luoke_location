e8c96d314a521ade9df24820e7cb83664f3d47cd
uzgene53 <wzj20010913@gmail.com>
2026-04-17 15:44:16 +0800

    Update MainActivity with capture trigger

--- android/app/src/main/java/MainActivity.kt
+++ android/app/src/main/java/MainActivity.kt
@@ -1,3 +1,35 @@
 package com.luoke.location
 
-class MainActivity {}
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.luoke.location.capture.ScreenCaptureManager
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var captureManager: ScreenCaptureManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val button = Button(this)
+        button.text = "Start Capture"
+        setContentView(button)
+
+        captureManager = ScreenCaptureManager(this)
+
+        button.setOnClickListener {
+            startActivityForResult(captureManager.createCaptureIntent(), 1001)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 1001 && captureManager.isCaptureResultOk(resultCode)) {
+            startService(Intent(this, TrackingService::class.java))
+        }
+    }
+}
