7da1e5072692d9b6af047a53022143c5116d8e5f
uzgene53 <wzj20010913@gmail.com>
2026-04-17 16:06:20 +0800

    Prepare Android app module for OpenCV and view binding

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,9 +14,14 @@ android {
         versionCode = 1
         versionName = "0.1"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
 }
