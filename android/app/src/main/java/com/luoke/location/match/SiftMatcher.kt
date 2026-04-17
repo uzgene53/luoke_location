e40d434e6c94b918acd30da71e68ba7e59051910
uzgene53 <wzj20010913@gmail.com>
2026-04-17 16:06:41 +0800

    Add SIFT matcher placeholder

---
+++ android/app/src/main/java/com/luoke/location/match/SiftMatcher.kt
@@ -0,0 +1,14 @@
+package com.luoke.location.match
+
+import android.graphics.Bitmap
+
+/**
+ * Placeholder for OpenCV SIFT matching
+ */
+class SiftMatcher {
+
+    fun match(minimap: Bitmap): Pair<Int, Int>? {
+        // TODO: integrate OpenCV SIFT
+        return null
+    }
+}
