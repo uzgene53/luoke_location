95d2f850030fec8ef130023ffa9002ec45685011
uzgene53 <wzj20010913@gmail.com>
2026-04-17 16:06:31 +0800

    Add screen image provider skeleton

---
+++ android/app/src/main/java/com/luoke/location/capture/ScreenImageProvider.kt
@@ -0,0 +1,14 @@
+package com.luoke.location.capture
+
+import android.graphics.Bitmap
+
+/**
+ * Skeleton for real screen capture pipeline (MediaProjection -> ImageReader -> Bitmap)
+ */
+class ScreenImageProvider {
+
+    fun getLatestFrame(): Bitmap? {
+        // TODO: replace with real MediaProjection + ImageReader pipeline
+        return null
+    }
+}
